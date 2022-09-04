461f74ca1e3da9e9976ec9aed1e0da4c1a2697a1
vncntcln <110927443+vncntcln@users.noreply.github.com>
2022-09-04 22:42:20 +0700

    Add files via upload

---
+++ 2.0 Strings/1.1 Strings.py
@@ -0,0 +1,13 @@
+# Backslash '\'
+
+print("Nany\'s House")
+print("\"Cool\"")
+
+# Newlines '\n'
+print("One \nTwo \nThree")
+
+# Tab '\t'
+print("\tThis is tab")
+
+# Escaping special character '\n'
+print("This is newlines \\n")
