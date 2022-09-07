c73ef57cb70581282d444920627251bc38804af9
vncntcln <110927443+vncntcln@users.noreply.github.com>
2022-09-07 23:53:42 +0700

    Add files via upload

---
+++ 3.0 Variables/1.3 Variable.py
@@ -0,0 +1,16 @@
+# Variable cannot written in special symbols / start with number
+
+# Overwrite variable
+
+age = 42
+age = 24 - 6
+print(age)
+
+
+a = 8
+name = "Bob"
+
+print(a * name)
+
+# del statementr can be used to remove variable
+del a
