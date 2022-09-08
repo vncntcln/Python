ceb76047063e4614f562bf32fb296b7ea70d7d04
vncntcln <110927443+vncntcln@users.noreply.github.com>
2022-09-08 22:58:04 +0700

    Add files via upload

---
+++ 3.0 Variables/4.3 F-Strings Superpowers.py
@@ -0,0 +1,14 @@
+# Operation within the {}
+a = 5
+b = 10
+print(f'Test if i can add {a} and {b} and have the result {a + b}.')
+
+# Add comma as separators
+number = 1234567890
+print(f'{number}')
+print(f'{number,}')
+print(f'{number:,}')
+
+# IF-Else Clause
+x = 1
+print(f"Lets test the value: {True if x == 1 else False}")
