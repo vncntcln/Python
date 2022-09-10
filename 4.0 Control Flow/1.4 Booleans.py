3a87622a5a68c965afe9ae7106f88ffeb0b69023
vncntcln <110927443+vncntcln@users.noreply.github.com>
2022-09-10 21:17:16 +0700

    Add files via upload

---
+++ 4.0 Control Flow/1.4 Booleans.py
@@ -0,0 +1,20 @@
+# Boolean comparing the value
+a = True
+print(a)
+
+print(2 == 3)
+
+print("Hello" == "Hello")
+
+# Comparison
+x = 7
+
+print(x != 8)
+print(x > 5)
+print(x < 2)
+print(x >= 7)
+print(x <= 7)
+
+# Represent integers as '1' and '0'
+x = (7 > 5)
+print(int(x))
---
+++ 4.0 Control Flow/2.4 If-Statements.py
@@ -0,0 +1,11 @@
+# If statements
+x = 42
+if x > 5:
+    print("x is greater than 5")
+
+# Nested If-Statements
+num = 12
+if num > 5:
+    print("Bigger than 5")
+    if num <= 47:
+        print("Between 5 and 47")
