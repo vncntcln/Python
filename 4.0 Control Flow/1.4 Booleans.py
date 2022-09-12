7cb18c257e0b5d7a1a2b45a2036213edca86de1d
vncntcln <110927443+vncntcln@users.noreply.github.com>
2022-09-12 15:18:05 +0700

    Add files via upload

--- 4.0 Control Flow/1.4 Booleans.py
+++ 4.0 Control Flow/1.4 Booleans.py
@@ -18,3 +18,33 @@ print(x <= 7)
 # Represent integers as '1' and '0'
 x = (7 > 5)
 print(int(x))
+
+# Boolean logic 'and'
+if (1 == 1) and (2 + 2 > 3):
+    print("true")
+else:
+    print("false")
+
+# Boolean logic 'or'
+print(1 == 1 or 2 == 2)
+
+print(1 == 1 or 2 == 3)
+
+print(1 != 1 or 2 == 2)
+
+print(2 < 1 or 3 > 6)
+
+# Boolean logic 'not'
+print(not 1 == 1)
+
+print(not 1 > 7)
+
+# Practice
+age = int(input())
+
+if (age > 0 and age <= 11):
+    print("Child")
+elif (age >= 12 and age <= 17):
+    print("Teen")
+else:
+    print("Adult")
