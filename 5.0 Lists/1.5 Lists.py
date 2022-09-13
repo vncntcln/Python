5380e28aa86f5b7df6266627a20318d137805c6e
vncntcln <110927443+vncntcln@users.noreply.github.com>
2022-09-13 22:30:45 +0700

    Add files via upload

---
+++ 5.0 Lists/1.5 Lists.py
@@ -0,0 +1,19 @@
+# Make lists
+words = ["Hello", "World", "!"]
+
+# Access lists using 'Square Brackets'
+print(words[0])
+print(words[1])
+print(words[2])
+
+m = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+# Access 'Specified row and column'
+print(m[1][2])
+
+# Index String
+str = "Hello world!"
+print(str[6])
---
+++ 5.0 Lists/2.5 List Operations.py
@@ -0,0 +1,5 @@
+# Re-assigned index of list
+nums = [5, 8, 2]
+nums[1] = 42
+
+print(nums)
